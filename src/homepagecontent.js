73322bc3c68b962b407f78f2dec020f61991035e
Michael Neveu <michaeln@myrtlemac.local>
2018-03-15 18:13:36 -0700

    homepage styling mostly complete

--- src/homepagecontent.js
+++ src/homepagecontent.js
@@ -1,10 +1,73 @@
 import React from 'react';
 
 
-var Homepagecontent = () => (
-  <div>
-    Home Page Content. Amazing. Truly Amazing.
-    <div className="home-page-box">
+var Homepagecontent = () => {
+  return (
+  <div className="homepage-grid-container">
+    
+  <div className="homepage-grid-container__item item-header header-titles-container">
+    <div className="header-titles-container__title">Rap Better</div>
+    <div className="header-titles-container__subheader-title">Here is risen the lyricism</div>
+    </div>
+
+  <div className="homepage-grid-container__item item-tool item-tool--layout">
+    <div className="grid-item--spacing">
+      <div className="large-button">
+        <div className="large-button__text">Rhyme</div>
+      </div>
+    </div>
+  </div>
+  <div className="homepage-grid-container__item item-tool--text item-tool-text">
+    Use this tool to fuel <br/>
+     your rhymes <br/>
+    & <br/>
+    to work your rythm 
+  </div>
+  <div className="homepage-grid-container__item item-tool-right item-tool--layout">
+    <div className="grid-item--spacing">
+      <div className="large-button--counterbalance-placeholder">
+        
+      </div>
+    </div>
+  </div>
+  <div className="homepage-grid-container__item item-videos">
+    {/*Video
+      background-image: url('./images/test2.jpg'); */}
+    <div className="video__images">
+    <div className="video__images--inner-boxed-div">
+      <div>
+        <img className="home-page-box-image video__images--specific-photo" src="./images/dono3.jpg"/>
+        <img className="home-page-box-image video__images--specific-photo" src="./images/dono2.jpg"/>
+        <img className="home-page-box-image video__images--specific-photo" src="./images/dono2.jpg"/>
+      </div>
+      <div>
+        <img className="home-page-box-image video__images--specific-photo" src="./images/dono1.jpg"/>
+        <img className="home-page-box-image video__images--specific-photo" src="./images/dono4.jpg"/>
+        
+      </div>
+      
+      </div>
+    </div>
+  </div>
+  <div className="homepage-grid-container__item item-videos--text item-videos-text">
+    <p>
+      Helpful Videos 
+      <br/>
+      &
+      <br/>
+      <span className="item-videos-text--course-text">COURSES</span>
+      <br/>
+    </p>
+  </div>
+  <div className="homepage-grid-container__item item-videos-right"></div>
+  <div className="homepage-grid-container__item item-footer">
+    <div className="grid-item--spacing item-footer__text">
+      &#169; 2018 Donovon Jenson
+    </div>
+  </div>
+  
+
+    {/*<div className="home-page-box">
     <div>
       <img className="home-page-box-image" src="http://lorempixel.com/output/abstract-q-c-300-300-7.jpg"/>
       </div>
@@ -24,9 +87,10 @@ var Homepagecontent = () => (
     </div>
     <div className="home-page-box">
       <img className="home-page-box-image" src="http://lorempixel.com/output/abstract-q-c-300-300-1.jpg"/>
-    </div>
+    </div>*/}
   </div>
   )
+}
 
 
 export default Homepagecontent;
