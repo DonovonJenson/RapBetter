708e0e8d9f73640bef12af366b4be64445a0d3d1
Donovon <donovonjenson@hotmail.com>
2018-03-30 17:05:58 -0600

    add place for course description, change image size and background

--- src/courses.jsx
+++ src/courses.jsx
@@ -3,14 +3,14 @@
 
 import React from 'react'
 // AUTO SCROLL DOWN
-var done = false;
-var currentScrollY;
-var scroll = setInterval(function(){ 
-  if (done === false) {
-    window.scrollBy(0,2); 
-    (currentScrollY === window.scrollY)? done = true : currentScrollY= window.scrollY;   
-  }
-}, 100);
+// var done = false;
+// var currentScrollY;
+// var scroll = setInterval(function(){ 
+//   if (done === false) {
+//     window.scrollBy(0,2); 
+//     (currentScrollY === window.scrollY)? done = true : currentScrollY= window.scrollY;   
+//   }
+// }, 100);
 
 var Courses = () => (
   <div className="inner-pages-title-center">
@@ -25,20 +25,42 @@ var Courses = () => (
     </div>
     <div className="main-flexbox-inner-pages">
       <div className="courses-flex">
-
-        <a href="https://www.udemy.com/write-your-first-rap-verse">
-          <img className="courses-image" src="./images/writeYourFirstVerseCourseImage.jpg"/>
-        </a>
-        <a href="https://www.udemy.com/writing-your-first-rap-song">
-          <img className="courses-image" src="./images/writeRecordFirstSongCourseImage.jpg"/>
-        </a>
-        <a href="https://www.udemy.com/rap-fundamentals">
-          <img className="courses-image" src="./images/rapmic.jpg"/>
-        </a>
+        <div className="single-course">
+          <a href="https://www.udemy.com/write-your-first-rap-verse" className='half'>
+            <img className="courses-image" src="./images/writeYourFirstVerseCourseImage.jpg"/>
+            <div className ="courses-flex-title"> 
+              Write Your First Verse - FREE
+            </div>
+          </a>
+          <div className="courses-explanation">
+            Eventually, stuff!
+          </div>
+        </div>
+        <div className="single-course">
+          <a href="https://www.udemy.com/writing-your-first-rap-song/?couponCode=STUDENTDISCOUNT" className='half'>
+            <img className="courses-image" src="./images/writeRecordFirstSongCourseImage.jpg"/>
+            <div className ="courses-flex-title"> 
+              Write & Record Your First Song - $10
+            </div>
+          </a>
+          <div className="courses-explanation">
+            Eventually, stuff!
+          </div>
+        </div>
+        <div className='single-course'>
+          <a href="https://www.udemy.com/rap-fundamentals/?couponCode=Youtubers" className='half'>
+            <img className="courses-image" src="./images/rapmic.jpg"/>
+            <div className ="courses-flex-title"> 
+              Rap Fundamentals - $15
+            </div>
+          </a>
+          <div className="courses-explanation">
+            Eventually, stuff!
+          </div>
+        </div>
       </div>
     </div>
     <div className="main-flexbox-inner-pages">
-      <div className="courses-flex__item--text">SIGN UP HERE</div>
     </div>
   </div>
 )
