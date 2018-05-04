dccf4caeb4b6f1088266bc3adfadd1636b812a7f
Thomas Cosby <cosbyts@gmail.com>
2018-05-04 08:54:29 -0700

    Refactored server app code
    - Set the www directory path as a constant variable since we reuse it in the express.static and for the /* route

--- server/app.js
+++ server/app.js
@@ -2,35 +2,37 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const _public = path.join(__dirname, '../www');
+
 
 // ---------- MOVE TO DB DIRECTORY --------------------
 
-var mongoose = require('mongoose');
-var db = mongoose.connection;
+// var mongoose = require('mongoose');
+// var db = mongoose.connection;
 
-//This line will need to be updated at deployment
-//mongoose.connect('mongodb://localhost/rapbetter');
-var mongooseURI = 'mongodb://' + (process.env.MONGODB_URI || 'localhost/rapbetter' )
+// //This line will need to be updated at deployment
+// //mongoose.connect('mongodb://localhost/rapbetter');
+// var mongooseURI = 'mongodb://' + (process.env.MONGODB_URI || 'localhost/rapbetter' )
 
-mongoose.connect(mongooseURI);
+// mongoose.connect(mongooseURI);
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('mongo connected!')
-});
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('mongo connected!')
+// });
 
-var rhymeSchema = mongoose.Schema ({
-  coreWord: {type: String, unique: true}, 
-  rhymeSet: Array
-})
+// var rhymeSchema = mongoose.Schema ({
+//   coreWord: {type: String, unique: true}, 
+//   rhymeSet: Array
+// })
 
-var Rhymeset = mongoose.model('Rhymeset',rhymeSchema)
+// var Rhymeset = mongoose.model('Rhymeset',rhymeSchema)
 
 // ----------------------------------------------------
  
 
  
-app.use(express.static(path.join(__dirname, '../www')));
+app.use(express.static(_public));
 
 app.get('/getRhyme', (req, res) =>{
   Rhymeset.aggregate(
@@ -41,12 +43,8 @@ app.get('/getRhyme', (req, res) =>{
   })
 })
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'www/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
+app.get('/*', (req, res) => {
+  res.sendFile(`${_public}/index.html`);
 })
 
 module.exports = app;
