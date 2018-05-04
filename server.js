165f5c347e331b6204101c11d60cfc49348b791c
Thomas Cosby <cosbyts@gmail.com>
2018-05-03 22:37:39 -0700

    Removed webpack-dev-middleware from server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
 const app = express();
 const path = require('path');
 
@@ -27,19 +25,8 @@ var rhymeSchema = mongoose.Schema ({
 var Rhymeset = mongoose.model('Rhymeset',rhymeSchema)
 
  
-const compiler = webpack(webpackConfig);
  
 app.use(express.static(__dirname + '/www'));
- 
-app.use(webpackDevMiddleware(compiler, {
-  hot: true,
-  filename: 'bundle.js',
-  publicPath: '/',
-  stats: {
-    colors: true,
-  },
-  historyApiFallback: true,
-}));
 
 app.get('/getRhyme', (req, res) =>{
   Rhymeset.aggregate(
