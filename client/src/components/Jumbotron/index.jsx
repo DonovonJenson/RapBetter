ac8cd61039634639956f97e03dbee38cee1f472d
Thomas Cosby <cosbyts@gmail.com>
2018-05-07 16:25:55 -0700

    create Jumbotron component
    - updated Home page with new Jumbotron component

---
+++ client/src/components/Jumbotron/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Jumbotron extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+
+      <div className="jumbotron"/>
+
+    );
+
+  }
+}
