4210845536b8407dd3f2e89b23bebac31330e806
Thomas Cosby <cosbyts@gmail.com>
2018-05-10 21:03:01 -0700

    added overflow to mobile description containers
    - overflow prevents the text from leaking down to the content below it
    - this commit sets up changes to how we are currently storing/displaying videos from youtube

--- client/src/pages/Videos.jsx
+++ client/src/pages/Videos.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 
+
 const Home = (props) => {
 
   return (
@@ -13,39 +14,36 @@ const Home = (props) => {
 
         <div className="video-player-wrapper">
 
-          <div className="video-player-current">
+          <div className="player-current-video">
             <YouTube videoId="ge0KWuwf1gA" opts={{height: '100%', width: '100%'}}/>
           </div>
 
-          <div className="video-player-current-description">
-            description
+          <div className="player-current-info-wrapper">
+            <h2 className="current-info-title">
+              Title: Placeholder title content
+            </h2>
+
+            <div className="current-info-description">
+              <div className="description-label">
+                Description
+              </div>
+              <div className="description-content">
+                Morbi elementum vestibulum risus, vel consectetur nunc interdum non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec cursus odio sed eleifend accumsan. Aenean dolor diam, hendrerit sit amet facilisis sed, imperdiet ut eros. Suspendisse sed convallis diam, ut fermentum enim. Pellentesque erat urna, ullamcorper euismod posuere in, molestie vitae arcu. Donec lectus diam, varius ac fermentum imperdiet, varius sit amet augue.
+              </div>
+            </div>
           </div>
 
         </div>
 
         <div className="videos-list-wrapper">
 
-          <div className="videos-list-entry">
-            Entry 1
-          </div>
-          <div className="videos-list-entry">
-            Entry 2
-          </div>
-          <div className="videos-list-entry">
-            Entry 3
-          </div>
-          <div className="videos-list-entry">
-            Entry 4
-          </div>
-          <div className="videos-list-entry">
-            Entry 5
-          </div>
-          <div className="videos-list-entry">
-            Entry 6
-          </div>
-          <div className="videos-list-entry">
-            Entry 7
-          </div>
+          {videos.map((video, index) => {
+            return (
+              <div key={`video-${index}-${video}`} className="videos-list-entry no-select">
+                <YouTube videoId={video} opts={{height: 'auto', width: '100%'}}/>
+              </div>
+            );
+          })}
 
         </div>
 
@@ -58,3 +56,26 @@ const Home = (props) => {
 }
 
 export default Home;
+
+
+// <div className="videos-list-entry">
+//   Entry 1
+// </div>
+// <div className="videos-list-entry">
+//   Entry 2
+// </div>
+// <div className="videos-list-entry">
+//   Entry 3
+// </div>
+// <div className="videos-list-entry">
+//   Entry 4
+// </div>
+// <div className="videos-list-entry">
+//   Entry 5
+// </div>
+// <div className="videos-list-entry">
+//   Entry 6
+// </div>
+// <div className="videos-list-entry">
+//   Entry 7
+// </div>
