cc1029deea03187c7186da7bbfbb999762076b59
Thomas Cosby <cosbyts@gmail.com>
2018-05-15 18:18:05 -0700

    Added feature that slices out links in description

--- client/src/components/VideoPlayer/index.jsx
+++ client/src/components/VideoPlayer/index.jsx
@@ -7,13 +7,52 @@ export default class VideoPlayer extends React.Component {
     this.state = {
       descriptionToggle: false
     };
+    this.buildDescriptionElement = this.buildDescriptionElement.bind(this);
+    this.buildDescriptionLink = this.buildDescriptionLink.bind(this);
+    this.renderDescription = this.renderDescription.bind(this);
     this.toggleDescriptionContainer = this.toggleDescriptionContainer.bind(this);
   }
 
-  toggleDescriptionContainer() {
+  buildDescriptionElement(string, key) {
+    return (
+      <span key={key}>
+        {string}
+        <br/>
+      </span>
+    );
+  }
 
-    // Since the description toggle is set to false by default, 
-    // the toggled class will only be applied the same time that the descriptionToggle is true
+  buildDescriptionLink(string, key) {
+    let sliceIndex = string.indexOf('http');
+    let text = string.slice(0, sliceIndex);
+    let link = string.slice(sliceIndex);
+    let remainder = '';
+
+    let testSplit = link.split(' ');
+
+    if (testSplit.length > 1) {
+      link = testSplit[0];
+      link = link.replace(/,/g, ' ');
+      remainder = testSplit.slice(1).join(' ');
+    }
+
+    return (
+      <span key={key}>
+        {text}<a className="description-link" href={link}>{link}</a>{remainder}
+        <br/>
+      </span>
+    );
+  }
+
+  renderDescription(description) {
+    return description.split('\n').map((item, key) => {
+      let linkRegex = /(http(s?))\:\/\//gi;
+      let linkToggle = linkRegex.test(item);
+      return linkToggle ? this.buildDescriptionLink(item, key) : this.buildDescriptionElement(item, key);
+    })
+  }
+
+  toggleDescriptionContainer() {
 
     let infoWrapper = document.getElementsByClassName('player-current-info-wrapper')[0];
     let descriptionContent = document.getElementsByClassName('description-content')[0];
@@ -57,16 +96,7 @@ export default class VideoPlayer extends React.Component {
 
           <div className="current-info-description">
             <div className="description-content">
-              {
-                description.split('\n').map((item, key) => {
-                  return (
-                    <span key={key}>
-                      {item}
-                      <br/>
-                    </span>
-                  )
-                })
-              }
+              {this.renderDescription(description)}
             </div>
           </div>
           {
